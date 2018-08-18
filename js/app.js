f7e63a692e523d6b5836fb2a10195bbeea00fb7d
Yara Castro <yaracastro24@gmail.com>
2018-08-18 15:55:04 -0500

    Master

---
+++ js/app.js
@@ -0,0 +1,74 @@
+/*
+ * Create a list that holds all of your cards
+ */
+ let allCards = document.querySelectorAll('.card');
+// Flip the cards & If cards match function............
+ let storageCards = [];
+ let pairedCards = [];
+
+// Shuffle Cards..................
+function shuffleCards() {
+    const deck = document.querySelector('.deck');
+    const randomCards = Array.from(document.querySelectorAll('.card li'));
+    const pickedCards = shuffle(randomCards);
+    for (card of pickedCards) {
+        deck.appendChild(card);
+    }
+} 
+    shuffleCards();
+
+ allCards.forEach(function(cardTarget){
+    cardTarget.addEventListener('click', event => { 
+
+        const clickCards = event.target;
+       if (clickCards.classList.contains('card') && storageCards.length < 2){
+           storageCards.push(cardTarget);
+           clickCards.classList.add('open','show');
+           
+        // Matching cards........
+        function matchCards(){
+        if (storageCards[0].firstElementChild.className === storageCards[1].firstElementChild.className){
+            storageCards[0].classList.add('match');
+            storageCards[1].classList.add('match');
+            storageCards = [];
+        }
+    }
+        // Remove if cards doesn't match......
+            if (storageCards.length === 2){
+                matchCards();
+                setTimeout(function(){
+                    storageCards.forEach(function(cardTarget){  
+                        cardTarget.classList.remove('open','show');
+                    });
+                   storageCards = []; 
+                }, 2000);
+                
+            }  
+        }
+    });
+ })
+
+ 
+
+// Set Timer.......
+
+// Set Score.......
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    array = Array.from(allCards);
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
